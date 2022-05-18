4859b34ae2066070a991aa860cd49d555f3ea687
Sarah Zhang <63278213+srazh@users.noreply.github.com>
2022-05-18 11:30:38 -0400

    js addons

--- app.js
+++ app.js
@@ -8,3 +8,14 @@ const weather = {};
 weather.temperature = {
     unit : "celsius"
 }
+
+// APP CONSTS AND VARS
+const KELVIN = 273;
+// API Key 
+const key = "82005d27a116c2880c8f0fcb866998a0";
+
+// Celsius to Fahrenheit conversion
+function celsiusToFahrenheit(temperature) {
+    return (temperature * 9/5)+ 32;
+}
+
