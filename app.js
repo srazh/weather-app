1e9f1579c7a2bf5b07daa6e608a46b48eac9c3c4
Sarah Zhang <63278213+srazh@users.noreply.github.com>
2022-05-23 16:12:41 -0400

    app.js add ons

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 // api key : 82005d27a116c2880c8f0fcb866998a0
 
 
-
-
-
-
 const notificationElement = document.querySelector(".notification");
 const iconElement = document.querySelector(".weather-icon");
 const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
 
 
 
+// Display weather to UI
+function displayWeater() {
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value} °<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+
+}
 
 
-
-
-
-
-
+    
+    
 
 
 // App data
